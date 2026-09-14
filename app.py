b4042aa8849cadd627e4ccfe9dc87bc93a34ecc0
Sikander Adeel Cheema <sikanderadeell271@gmail.com>
2026-09-14 06:25:37 +0100

    Connecting Streamlit

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
-import streamlit as st 
+import streamlit as st
+from database import get_products
+
+
+st.set_page_config(page_title="One Stop Coffee")
 
 st.title("One Stop Coffee")
-st.write("Welcome To One Stop Coffee! I Am Your Digital Barista - Let Me Show You What We Are Brewing")
+st.write("Welcome! I Am Your Digital Barista - Let's Get Your Day Started In Style :)")
+
+
+st.subheader("Our Brew")
+
+products = get_products()
+menu_rows = []
+
+
+for product in products:
+
+    if product["stock_quantity"] > 0:
+        availability = "Available"
+    else:
+        availability = "Out of Stock"
+
+        menu_rows.append({
+            "Product": product["name"],
+            "Category": product["category"],
+            "Price": f"£{product['price_pence'] / 100:2f}",
+            "Availability": availability,
+            "Description": product["description"],
+        })
+
+        st.dataframe(menu_rows, hide_index=True)
